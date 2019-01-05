525aa8af2059dd36426a8854db7c536834b4aefc
Ruan Silva <ruanluiz.as@gmail.com>
2019-01-05 17:06:54 -0200

    first commit

---
+++ app/src/main/java/com/example/ruan/ceep/ui/activity/ListaNotasActivity.java
@@ -0,0 +1,15 @@
+package com.example.ruan.ceep.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ruan.ceep.R;
+
+public class ListaNotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_notas);
+    }
+}
