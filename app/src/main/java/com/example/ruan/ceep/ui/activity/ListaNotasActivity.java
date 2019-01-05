3c7aa7c0a2fc6f92bd37e03e2c8496489445f2a9
Ruan Silva <ruanluiz.as@gmail.com>
2019-01-05 18:19:38 -0200

    aula 02

--- app/src/main/java/com/example/ruan/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/com/example/ruan/ceep/ui/activity/ListaNotasActivity.java
@@ -2,8 +2,15 @@ package com.example.ruan.ceep.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 import com.example.ruan.ceep.R;
+import com.example.ruan.ceep.dao.NotaDAO;
+import com.example.ruan.ceep.model.Nota;
+import com.example.ruan.ceep.ui.adapter.ListaNotasAdapter;
+
+import java.util.List;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
@@ -11,5 +18,19 @@ public class ListaNotasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
+
+        ListView listaNotas = findViewById(R.id.listView);
+
+        NotaDAO dao = new NotaDAO();
+        for (int i = 1; i <= 10000; i++){
+            dao.insere(
+                    new Nota("Título "+ i, "Descrição " + i)
+            );
+        }
+
+        List<Nota> todasNotas = dao.todos();
+
+        ListaNotasAdapter adapter = new ListaNotasAdapter(this, todasNotas);
+        listaNotas.setAdapter(adapter);
     }
 }
